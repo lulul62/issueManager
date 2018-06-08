a30c2dd27b4d8a0d838381c780be72e8656b83a0
Leplan Ludwig <leplan.ludwig@gmail.com>
2018-06-08 23:11:56 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,176 @@
-<!DOCTYPE html><html><head><meta charset=utf-8><meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"><title>CSINDICATORS</title><script src=https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js></script><link href=/static/css/app.0d1ae62aab458916cfaacdd357087f38.css rel=stylesheet></head><body><div id=app></div><script type=text/javascript src=/static/js/manifest.c8ba58bd0b005776e11d.js></script><script type=text/javascript src=/static/js/vendor.bdec0c657794e6f7baf1.js></script><script type=text/javascript src=/static/js/app.4b75bd68063b60fb2c59.js></script></body></html>
+<!DOCTYPE html>
+<html >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.6.5, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.6.5, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo-soprasteria-1-600x255.png" type="image/x-icon">
+  <meta name="description" content="">
+  <title>Home</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+  <section class="menu cid-qUp4bk9jcv" once="menu" id="menu2-8">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="https://mobirise.com">
+                        <img src="assets/images/logo-soprasteria-1-600x255.png" alt="Mobirise" title="" style="height: 3.8rem;">
+                    </a>
+                </span>
+                
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-black display-4" href="https://mobirise.com"></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-black display-4" href="index.html#video2-2"></a>
+                </li><li class="nav-item"><a class="nav-link link text-black display-4" href="assets/files/CDS-INDICATORS.pdf"><span class="mbri-image-gallery mbr-iconfont mbr-iconfont-btn"></span>Brochure de présentation</a></li><li class="nav-item"><a class="nav-link link text-black display-4" href="index.html#video2-2"><span class="mbri-preview mbr-iconfont mbr-iconfont-btn"></span>Vidéo de présentation</a></li></ul>
+            
+        </div>
+    </nav>
+</section>
+
+<section class="engine"><a href="https://mobirise.ws/k">develop your own website</a></section><section class="header12 cid-qUjdJdTrXR mbr-fullscreen mbr-parallax-background" id="header12-1">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.9; background-color: rgb(70, 80, 82);">
+    </div>
+
+    <div class="container  ">
+            <div class="media-container">
+                <div class="col-md-12 align-center">
+                    <h1 class="mbr-section-title pb-3 mbr-white mbr-bold mbr-fonts-style display-1">CDS-INDICATORS</h1>
+                    <p class="mbr-text pb-3 mbr-white mbr-fonts-style display-5">Web-application de monitoring projet à destination des centres de services.</p>
+                    <div class="mbr-section-btn align-center py-2"><a class="btn btn-md btn-white-outline display-4" href="mailto:ludwig.leplan2@soprasteria.com"><span class="mbri-paper-plane mbr-iconfont mbr-iconfont-btn"></span>Demander une démonstration</a></div>
+
+                    <div class="icons-media-container mbr-white">
+                        <div class="card col-12 col-md-6 col-lg-3">
+                            <div class="icon-block">
+                            <a href="https://mobirise.com/">
+                                <span class="mbr-iconfont mbri-preview"></span>
+                            </a>
+                            </div>
+                            <h5 class="mbr-fonts-style display-5">Alerte des jalons en retards</h5>
+                        </div>
+
+                        <div class="card col-12 col-md-6 col-lg-3">
+                            <div class="icon-block">
+                                <a href="https://mobirise.com/">
+                                    <span class="mbr-iconfont mbri-bookmark"></span>
+                                </a>
+                            </div>
+                            <h5 class="mbr-fonts-style display-5">Rapport d'avancement</h5>
+                        </div>
+
+                        <div class="card col-12 col-md-6 col-lg-3">
+                            <div class="icon-block">
+                                <a href="https://mobirise.com/">
+                                    <span class="mbr-iconfont mbri-growing-chart"></span>
+                                </a>
+                            </div>
+                            <h5 class="mbr-fonts-style display-5">Calcul de performance</h5>
+                        </div>
+
+                        <div class="card col-12 col-md-6 col-lg-3">
+                            <div class="icon-block">
+                                <a href="https://mobirise.com/">
+                                    <span class="mbr-iconfont mbri-desktop"></span>
+                                </a>
+                            </div>
+                            <h5 class="mbr-fonts-style display-5">Suivi opérationnel recette, build et run</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+        <a href="#next">
+            <i class="mbri-down mbr-iconfont"></i>
+        </a>
+    </div>
+</section>
+
+<section class="mbr-section content4 cid-qUp2H5yMKS" id="content4-7">
+
+    
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="title col-12 col-md-8">
+                <h2 class="align-center pb-3 mbr-fonts-style display-2">Liberez vous des contraintes.</h2>
+                <h3 class="mbr-section-subtitle align-center mbr-light mbr-fonts-style display-5">Afin de gagner en agilité.</h3>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="cid-qUjhI9tkCM" id="video2-2">
+
+    
+    
+    <figure class="mbr-figure align-center container">
+        <div class="video-block" style="width: 100%;">
+            <div><iframe class="mbr-embedded-video" src="https://www.powtoon.com/embed/evZyfPPQhsc/?loop=1&amp;autoplay=1" width="1280" height="720" frameborder="0" allowfullscreen></iframe></div>
+        </div>
+    </figure>
+</section>
+
+<section once="" class="cid-qUp7uQktaG" id="footer6-9">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="col-12">
+                <p class="mbr-text mb-0 mbr-fonts-style display-7">Delivering transformation. Together.<br></p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+</body>
+</html>
